154b37e3df866838b9ead686cdc5b060f70b50e8
Zanna Fitch <zannafitch2004@gmail.com>
2023-09-25 10:44:49 -0500

    Feat: create a connection to API

---
+++ app/services/open_library_service.rb
@@ -0,0 +1,17 @@
+class OpenLibraryService
+
+  def self.get_books(location, quantity)
+    get_url("search.json?q=#{location}&page=2&limit=#{quantity}")
+  end
+
+  def self.get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.conn
+    Faraday.new(url: "https://openlibrary.org/") 
+  end
+end
+
+# https://openlibrary.org/search.json?q=denver,co&page=2&limit=5
