93ecec93e091162517ccf1d466765e549dc32035
Zanna Fitch <zannafitch2004@gmail.com>
2023-09-25 11:13:51 -0500

    fix: model tests

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Forecast, type: :model do
 
   describe 'class methods' do
     describe '.correct_quantity' do 
-      it ' will raise an error if the quantity is <= 0' do 
+      it ' will pass if quantity is greater than 0' do 
         expect(Book.correct_quantity(1)).to eq(1)
       end
     end
