dee93df243d9182098633b616defaca58019e7e2
Zanna Fitch <zannafitch2004@gmail.com>
2023-09-25 12:42:17 -0500

    Spec: tests for Poro method

--- spec/poros/book_object_spec.rb
+++ spec/poros/book_object_spec.rb
@@ -13,4 +13,19 @@ RSpec.describe Book do
     expect(book.isbn).to eq(['0762507845'])
     expect(book.title).to eq('Spot')
   end
+
+  context "check_isbn" do 
+    it ' will assign An array if the isbn in nil' do 
+      attributes = {
+        isbn: nil,
+        title: 'Spot'
+      }
+  
+      book = BookObject.new(attributes)
+    
+      expect(book).to be_a(BookObject)
+      expect(book.isbn).to eq(['NA'])
+      expect(book.title).to eq('Spot')
+    end
+  end
 end
